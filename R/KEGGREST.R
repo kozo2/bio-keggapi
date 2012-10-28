3edfacb61c2320ab790d7710f8b6c7718e1906f8
Kozo Nishida <knishida@riken.jp>
2012-10-29 04:27:16 +0900

    print requested REST url for message

--- R/KEGGREST.R
+++ R/KEGGREST.R
@@ -9,6 +9,7 @@ convertTable <- function(res){
 
 list.organisms <- function(){ 
   url <- "http://rest.kegg.jp/list/organism"
+  print(url)
   response <- getURL(url)
   table <- convertTable(response)
   table
@@ -16,6 +17,7 @@ list.organisms <- function(){
 
 list.pathways <- function(org){
   url <- paste("http://rest.kegg.jp/list/pathway", org, sep="/")
+  print(url)
   response <- getURL(url)
   table <- convertTable(response)
   table
@@ -23,6 +25,7 @@ list.pathways <- function(org){
 
 bconv <- function(target.db, id.list){
   url <- paste("http://rest.kegg.jp/conv", target.db, paste(id.list, collapse="+"), sep="/")
+  print(url)
   response <- getURL(url)
   table <- convertTable(response)
   table
@@ -30,12 +33,14 @@ bconv <- function(target.db, id.list){
 
 bget <- function(dbentries, option=""){
   url <- paste("http://rest.kegg.jp/get", paste(dbentries, collapse="+"), option, sep="/")
+  print(url)
   response <- getURL(url)
   response
 }
 
 get.genes.by.pathway <- function(pathway.id){
   url <- paste("http://rest.kegg.jp/link", substr(pathway.id, 6, 8), pathway.id, sep="/")
+  print(url)
   response <- getURL(url)
   table <- convertTable(response)
   table
