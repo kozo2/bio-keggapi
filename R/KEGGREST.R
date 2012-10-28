1c6771f9313f8e10d444dc375b0d14527ef6b2ce
Kozo Nishida <knishida@riken.jp>
2012-10-29 00:26:47 +0900

    added helper for format convert

--- R/KEGGREST.R
+++ R/KEGGREST.R
@@ -1,17 +1,20 @@
 library("RCurl")
 
-list.organisms <- function(){
-  response <- getURL("http://rest.kegg.jp/list/organism")
-  rows <- strsplit(response, "\n")
+convertTable <- function(res){
+  rows <- strsplit(res, "\n")
   rows.len <- length(rows[[1]])
   result <- matrix(unlist(lapply(rows, strsplit, "\t")), nrow=rows.len, byrow=T)
   result
 }
 
+list.organisms <- function(){
+  response <- getURL("http://rest.kegg.jp/list/organism")
+  table <- convertTable(response)
+  table
+}
+
 list.pathways <- function(org){
-  response <- getURL(paste("http://rest.kegg.jp/list/pathway/", org))
-  rows <- strsplit(response, "\n")
-  rows.len <- length(rows[[1]])
-  result <- matrix(unlist(lapply(rows, strsplit, "\t")), nrow=rows.len, byrow=T)
-  result
+  response <- getURL(paste("http://rest.kegg.jp/list/pathway", org, sep="/"))
+  table <- convertTable(response)
+  table
 }
