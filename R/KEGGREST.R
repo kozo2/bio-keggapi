285e904685fe2589adad319b9d0d5f0dafc7711b
Kozo Nishida <knishida@riken.jp>
2012-10-28 19:43:36 +0900

    list.pathways returns a matrix

--- R/KEGGREST.R
+++ R/KEGGREST.R
@@ -7,3 +7,11 @@ list.organisms <- function(){
   result <- matrix(unlist(lapply(rows, strsplit, "\t")), nrow=rows.len, byrow=T)
   result
 }
+
+list.pathways <- function(org){
+  response <- getURL(paste("http://rest.kegg.jp/list/pathway/", org))
+  rows <- strsplit(response, "\n")
+  rows.len <- length(rows[[1]])
+  result <- matrix(unlist(lapply(rows, strsplit, "\t")), nrow=rows.len, byrow=T)
+  result
+}
